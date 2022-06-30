b4e96a2fb2a8dcee245cf907245f99f885e6f1c9
Inna <innasilivonchik@mail.ru>
2022-06-30 20:45:38 +0300

    Решение задачи 6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2==0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
