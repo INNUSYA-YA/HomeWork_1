c1131b23cb012e6aeab1d2e3928cda2182e1c86b
Inna <innasilivonchik@mail.ru>
2022-06-30 20:31:54 +0300

    Решение задачи 4

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("ВВедите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    if(a>c)
+    Console.WriteLine("Наибольшее число" + "-" + a);
+    else
+    {
+        Console.WriteLine("Наибольшее число" + "-" + c);
+    }
+}
+else
+{
+    if(b>c)
+    Console.WriteLine("Наибольшее число" + "-" + b);
+    else
+    {
+         Console.WriteLine("Наибольшее число" + "-" + c);
+    }
+}
