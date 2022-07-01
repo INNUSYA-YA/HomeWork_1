6a13ab8e50a81c3dbca5322a9c6030e9744cd26a
Inna <innasilivonchik@mail.ru>
2022-07-01 07:36:22 +0300

    Решение задачи 8

---
+++ Task8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("ВВедите любое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a-(a-1);
+string c = " ";
+while(b<=a)
+{
+    if(b%2==0)
+    {
+       c = c + " " + b;
+       b=b+2;
+        }
+    else
+       {
+          b=b+1; 
+              }
+    
+  }
+    
+Console.WriteLine(c);
